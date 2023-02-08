81743f3716778bc6e97e52dac9800f150eeb3d4b
arnavnarain <narain.a@northeastern.edu>
2023-02-08 09:33:21 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialCityState = { city: "", state: "" };
+  const [cityState, setCityState] = useState(initialCityState);
+  const [zipcode, setZipcode] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>City/State Lookup Tool</h1>
+      <form action="" className="form-data">
+        <label htmlFor="zip">Type Zip Code Here</label>
+        <input
+          className="zip"
+          value={zipcode}
+          placeholder="XXXXX"
+          type="text"
+          name="zip"
+          id="zip"
+        />
+        <label htmlFor="city">City</label>
+        <input
+          className={`city`}
+          value={cityState.city}
+          type="text"
+          name="city"
+          disabled
+          id="city"
+        />
+        <label htmlFor="state">State</label>
+        <input
+          className={`state`}
+          value={cityState.state}
+          type="text"
+          name="state"
+          disabled
+          id="state"
+        />
+      </form>
+      <pre>
+        <code>
+          {JSON.stringify({
+            zipcode: zipcode,
+            city: cityState.city,
+            state: cityState.state,
+          })}
+        </code>
+      </pre>
     </div>
   );
 }
